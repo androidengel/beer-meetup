6b262159b0c8b88912471524e7985fc8dd8901c9
Andrew Engel <androidengel@gmail.com>
2020-01-19 15:36:42 -0600

    setting up eslint for backend

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {
   BrowserRouter, Route, Switch,
 } from 'react-router-dom';
@@ -10,21 +10,38 @@ import meetings from '../pages/meetings';
 import members from '../pages/members';
 import account from '../pages/account';
 
-function App() {
-  return (
-    <BrowserRouter>
-      <Header />
-      <Page>
-        <Switch>
-          <Route exact path="/" component={home} />
-          <Route exact path="/beers" component={beers} />
-          <Route exact path="/meetings" component={meetings} />
-          <Route exact path="/members" component={members} />
-          <Route exact path="/account" component={account} />
-        </Switch>
-      </Page>
-    </BrowserRouter>
-  );
+class App extends Component {
+  state = {
+    backendResponse: '',
+  }
+
+  callBackend = () => {
+    fetch('http://localhost:8000/test')
+      .then(res => res.text())
+      .then(res => this.setState({backendResponse: res}))
+  }
+
+  componentDidMount() {
+    this.callBackend();
+  }
+
+  render() {
+    return (
+      <BrowserRouter>
+        {/* <p>{this.state.backendResponse}</p> */}
+        <Header />
+        <Page>
+          <Switch>
+            <Route exact path="/" component={home} />
+            <Route exact path="/beers" component={beers} />
+            <Route exact path="/meetings" component={meetings} />
+            <Route exact path="/members" component={members} />
+            <Route exact path="/account" component={account} />
+          </Switch>
+        </Page>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
