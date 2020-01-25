b3d5781629b46eb6b8a31ce7af4f85994d4789a6
Andrew Engel <androidengel@gmail.com>
2020-01-25 17:36:20 -0600

    styling request member form

--- frontend/src/components/RequestMember.js
+++ frontend/src/components/RequestMember.js
@@ -1,21 +1,24 @@
 import React from 'react';
 
 const RequestMember = () => (
-  <form className="card">
-    <label htmlFor="name">
-      Name
-      <input type="text" id="name" name="name" required />
-    </label>
-    <label htmlFor="email">
-      Email
-      <input type="email" id="email" name="email" />
-    </label>
-    <label htmlFor="description">
-      What makes this person a good fit?
-      <input type="textarea" id="description" name="description" />
-    </label>
-    <input type="submit" value="Submit Request" />
-  </form>
+  <div>
+    <h2>New Member Request</h2>
+    <form className="card">
+      <label htmlFor="name">
+        Name
+        <input type="text" id="name" name="name" required />
+      </label>
+      <label htmlFor="email">
+        Email
+        <input type="email" id="email" name="email" />
+      </label>
+      <label htmlFor="description">
+        What makes this person a good fit?
+        <textarea id="description" name="description" />
+      </label>
+      <input type="submit" value="Submit Request" className="button" />
+    </form>
+  </div>
 );
 
 export default RequestMember;
--- frontend/src/components/styles/GlobalStyles.js
+++ frontend/src/components/styles/GlobalStyles.js
@@ -24,6 +24,12 @@ const GlobalStyles = createGlobalStyle`
     font-size: 1.5rem;
     line-height: 2;
   }
+  h2 {
+    text-align: center;
+    margin: 0;
+    font-size: 3rem;
+    letter-spacing .25rem;
+  }
   a {
     text-decoration: none;
     color: ${(props) => props.theme.offWhite};
@@ -32,15 +38,45 @@ const GlobalStyles = createGlobalStyle`
     margin: 0 auto;
     width: 70%;
     text-align: left;
+
   }
   label {
     font-size: 1.6rem;
-    letter-spacing: 1.5px;
+    letter-spacing: 1px;
+    font-weight: 700;
   }
-  input {
+  input, textarea {
+    box-sizing: border-box;
     width: 100%;
     padding: .5rem;
     margin: 1rem auto;
+  }
+  textarea {
+    height: 9rem;
+    overflow: auto;
+    resize: none;
+    font-family: arial;
+  }
+  .card {
+    background: #252628;
+    padding: 5rem;
+    border-radius: 1%;
+    border: 1px solid #383a3d;
+    box-shadow: 0 12px 10px rgba(0,0,0,0.9);
+    @media (max-width: 700px) {
+      padding: 3rem;
+    } 
+  }
+  .button {
+    background: ${(props) => props.theme.yellow};
+    color: ${(props) => props.theme.black};
+    font-weight: 900;
+    width: auto;
+    border: 0;
+    &:hover {
+      background: #d6ab1c;
+      cursor: pointer;
+    }
   }
 `;
 
