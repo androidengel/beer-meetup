84539ff72a58e7ea016911d5f5a9fa019728cd10
Andrew Engel <androidengel@gmail.com>
2020-01-25 08:05:47 -0600

    adding Link from react router dom to request member button

--- frontend/src/pages/members.js
+++ frontend/src/pages/members.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const members = () => (
   <div>
     <h1>MEMBERS</h1>
-    <button type="button">+Request Member</button>
+    <Link to="/members/request">
+      <button type="button">+Request Member</button>
+    </Link>
   </div>
 );
 
