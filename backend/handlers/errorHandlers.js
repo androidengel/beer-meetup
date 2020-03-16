e045092aefeeed7f39ca6b4376e4d8e864cbbaa4
Andrew Engel <androidengel@gmail.com>
2020-03-16 06:52:45 -0500

    adding notFound function

--- backend/handlers/errorHandlers.js
+++ backend/handlers/errorHandlers.js
@@ -3,3 +3,11 @@
 exports.catchErrors = (fn) => function (req, res, next) {
   return fn(req, res, next).catch(next);
 };
+
+// when hitting an page that is not found
+// mark as 404 and pass it along to error handler to display
+exports.notFound = (req, res, next) => {
+  const err = new Error('Not Found');
+  err.status('404');
+  next(err);
+};
