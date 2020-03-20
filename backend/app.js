bf4080854e2e529b5e5cba1851b5257d9c0d29c0
Andrew Engel <androidengel@gmail.com>
2020-03-20 07:24:14 -0500

    adding connect-mongo module

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,8 @@
 const createError = require('http-errors');
 const express = require('express');
-const expressSession = require('express-session');
+const session = require('express-session');
+const mongoose = require('mongoose');
+const MongoStore = require('connect-mongo')(session);
 const passport = require('passport');
 const flash = require('connect-flash');
 const path = require('path');
@@ -24,12 +26,25 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+// app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({
+  secret: process.env.SECRET,
+  key: process.env.KEY,
+  resave: false,
+  saveUninitialized: false,
+  store: new MongoStore({ mongooseConnection: mongoose.connection }),
+}));
 
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash());
+
+app.use((req, res, next) => {
+  res.locals.flashes = req.flash();
+  next();
+});
 
 // after all middleware, handle routes
 app.use('/', router);
